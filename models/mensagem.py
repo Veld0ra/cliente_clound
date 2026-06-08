f178b825e4e6fddfa0935f9fc5cae539c12690aa
Sofia <143623338+Veld0ra@users.noreply.github.com>
2026-06-08 19:33:35 -0300

    Delete models directory

--- models/mensagem.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Mensagem(BaseModel):
-    texto: str
-    id: int | None = None
-    nome: str = ""
-    pronome: str = ""
-    memoria: str = ""
-    tipo: str = "usuario"
